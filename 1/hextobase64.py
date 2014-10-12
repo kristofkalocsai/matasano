d22a453b9dca15c94a29c73cf0650ff4a1c1e64f
kristofkalocsai <kristofkalocsai@gmail.com>
2014-10-12 18:16:16 +0200

    first iteration

--- 1/hextobase64.py
+++ 1/hextobase64.py
@@ -1,6 +1,19 @@
 def hextobase64(hexstring):
     hexlist=[ord(c) for c in hexstring]                 #make a list of character orders(ascii codes)
     binlist=[bin(l)[2:].zfill(8) for l in hexlist]      #convert the list to binary values
-    binary=''.join(binlist)                             #concat the list so is is one string
-    index=[]
-    return binary
+    binary=''.join(binlist)                             #concat the list so it is one string
+    index='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    base64=''
+    charbuff = ''
+    for c in binary:
+        charbuff += c
+        # print len(charbuff)
+        # print charbuff
+        if len(charbuff) == 6:
+            base64 += index[int(charbuff,2)]
+            charbuff = ''
+    if (len(binary)/8)% 3 == 1:
+        base64 += '=='
+    if (len(binary)/8)% 3 == 2:
+        base64 += '='
+    return base64

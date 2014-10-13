8e77ae3ee2da6525d301f99b2589725534929051
kristofkalocsai <kristofkalocsai@gmail.com>
2014-10-13 11:22:47 +0200

    challenge 2
    first iteration

---
+++ 2/fixedxor.py
@@ -0,0 +1,6 @@
+import sys
+def fixedxor(str1, str2):
+    if len(str1) == len(str2):
+        return hex(int(str1, 16) ^ int(str2, 16))[2:].rstrip("L")
+    else:
+        print 'input lengths not equal'

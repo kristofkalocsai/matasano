9925a4e63936f2d7d0e3b93c85f6ed3bdf47603c
kristofkalocsai <kristofkalocsai@gmail.com>
2014-11-12 11:35:41 +0100

    first fix, still not perfect

--- 6/singlebxorcipher.py
+++ 6/singlebxorcipher.py
@@ -1,24 +1,28 @@
 import fixedxor
-alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 '
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ,.-'" + '"'
+
+
 def sbxorcip(string):
-    messagelist={}
+    messagelist = {}
     string = string.strip()
     for c in alphabet:
-        keycandid=''
-        for x in range(0,(len(string)/2)+2):
+        keycandid = ''
+        for x in range(0, (len(string) / 2) + 2):
             keycandid += str(hex(ord(c))[2:])
         if len(keycandid) != len(string):
             keycandid = keycandid[0:(len(string))]
-        decodedhex=fixedxor.fixedxor(string,keycandid)
-        charbuff=''
-        message=''
+        decodedhex = fixedxor.fixedxor(string, keycandid)
+        charbuff = ''
+        message = ''
         for c in decodedhex:
             charbuff += c
             if len(charbuff) == 2:
-                message += chr(int(charbuff,16))
+                message += chr(int(charbuff, 16))
                 charbuff = ''
         messagelist[message] = score(message)
     return max(messagelist, key=messagelist.get)
+
 
 def score(string):
     score = 0.0
